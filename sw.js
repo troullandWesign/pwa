2f04ce82dd738fb0be8156f0ebe41fe73359e7dd
ROULLAND <117265399+troullandWesign@users.noreply.github.com>
2024-01-09 13:48:03 +0100

    azazaza

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const APP_STATIC_RESOURCES = [
     "/style.css",
     "/app.js",
     "/manifest.json",
-    "/icons/icon.png",
+    "/icons/circle.svg",
 ];
 
 self.addEventListener("install", (event) => {
