9c1a3f85a7b4dae3194c0a998900ab7cfc5d4635
ROULLAND <117265399+troullandWesign@users.noreply.github.com>
2024-01-09 12:29:53 +0100

    push

--- sw.js
+++ sw.js
@@ -1,68 +1,60 @@
-// La version du cache
 const VERSION = "v1";
+const CACHE_NAME = `pwa-poc-${VERSION}`;
 
-// Le nom du cache
-const CACHE_NAME = `period-tracker-${VERSION}`;
-
-// Les ressources statiques nécessaires au fonctionnement de l'application
 const APP_STATIC_RESOURCES = [
-  "/",
-  "/index.html",
-  "/app.js",
-  "/styles.css",
-  "/manifest.json",
-  "/icon.png",
+    "/",
+    "/index.html",
+    "/style.css",
+    "/app.js",
+    "/manifest.json",
+    "/icons/wheel.svg",
+    "/icon.png",
 ];
 
-// Lors de l'installation, on met en cache les ressources statiques
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      cache.addAll(APP_STATIC_RESOURCES);
-    })(),
-  );
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            cache.addAll(APP_STATIC_RESOURCES);
+        })(),
+    );
 });
 
-// Lors de l'activation, on supprime les anciens caches
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    (async () => {
-      const names = await caches.keys();
-      await Promise.all(
-        names.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        }),
-      );
-      await clients.claim();
-    })(),
-  );
+    event.waitUntil(
+        (async () => {
+            const names = await caches.keys();
+            await Promise.all(
+                names.map((name) => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                }),
+            );
+            await clients.claim();
+        })(),
+    );
 });
 
-// Lors de la récupération des ressources, on intercepte les
-// requêtes au serveur et on répond avec les réponses en cache
-// plutôt que de passer par le réseau
 self.addEventListener("fetch", (event) => {
-  // Notre application n'a qu'une seule page,
-  // on n'utilisera que celle-ci.
-  if (event.request.mode === "navigate") {
-    event.respondWith(caches.match("/"));
-    return;
-  }
+    // Lorsqu'on cherche une page HTML
+    if (event.request.mode === "navigate") {
+        // On renvoie à la page index.html
+        event.respondWith(caches.match("/"));
+        return;
+    }
 
-  // Pour toutes les autres requêtes, on passera par le cache
-  event.respondWith(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      const cachedResponse = await cache.match(event.request.url);
-      if (cachedResponse) {
-        // On renvoie la réponse mise en cache si elle y est disponible
-        return cachedResponse;
-      }
-      // Si la ressource n'est pas dans le cache, on renvoie une 404.
-      return new Response(null, { status: 404 });
-    })(),
-  );
+    // Pour tous les autres types de requête
+    event.respondWith(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            const cachedResponse = await cache.match(event.request.url);
+            if (cachedResponse) {
+                // On renvoie la réponse mise en cache si elle est disponible.
+                return cachedResponse;
+            }
+            // On répond avec une réponse HTTP au statut 404.
+            return new Response(null, { status: 404 });
+        })(),
+    );
 });
